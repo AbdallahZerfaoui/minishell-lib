880f00dcc577ece764260f3f90f996ef70141ca6
AbdallahZerfaoui <82400944+AbdallahZerfaoui@users.noreply.github.com>
2025-03-02 20:05:02 +0100

    Update gc_track.c

--- g_collector/gc_track.c
+++ g_collector/gc_track.c
@@ -6,7 +6,7 @@
 /*   By: azerfaou <azerfaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 12:52:37 by azerfaou          #+#    #+#             */
-/*   Updated: 2025/03/02 19:59:31 by azerfaou         ###   ########.fr       */
+/*   Updated: 2025/03/02 20:04:55 by azerfaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	gc_error(const char *msg, char *file, int line, uint8_t exit_stat)
 	ft_putstr_fd(" line ", 2);
 	ft_putnbr_fd(line, 2);
 	ft_putstr_fd(": ", 2);
-	ft_putstr_fd(msg, 2);
+	ft_putstr_fd((char *)msg, 2);
 	ft_putstr_fd("\n", 2);
 	gc_shutdown();
 }
